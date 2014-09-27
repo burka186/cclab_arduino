422110144c5accfc8f39bf9378a28834c9ffd12b
burka186 <avamburke@Avas-MacBook-Air.local>
2014-09-27 15:59:28 -0400

    debouncing homework working code and running video

--- homework_debouncing/homework_debouncing.ino
+++ homework_debouncing/homework_debouncing.ino
@@ -1,48 +1,55 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
+const int buttonPin = 2;    // the number of the button pin
+const int ledPin = 13;      // the number of the LED pin
 
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin 13. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://arduino.cc
+//these variables will change
+int ledState = HIGH;         // the current state of the output (LED) pin
+int buttonState;             // the current reading from the input (button) pin
+int lastButtonState = LOW;   // the previous reading from the input (button) pin
 
-  This example code is in the public domain.
+//set debounce:
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 50;    // the debounce time (to avoid flickering/noise)
 
-  modified 8 May 2014
-  by Scott Fitzgerald
- */
- //Pin 13 has an LED connected on most Arduino boards
- //give it a name:
-int led = 13;
-
-//define the button
-int buttonPin = 2;
-//define the state of the button
-int buttonState = 0;
-
-// the setup function runs once when you press reset or power the board
 void setup() {
-  //start the serial connection from Arduino back to computer (serial is like console.log in Javascript)
-  Serial.begin(9600);
-  
-  // initialize digital pin 13 (defined as led) as an output.
-  pinMode(led, OUTPUT);
+  //set button as input and LED as output:
   pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+
+  // set initial LED state
+  digitalWrite(ledPin, ledState);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  //read the button (gets the state of the button pin (on or off)
-  buttonState = digitalRead(buttonPin);
-    //perform different actions depending on state of button:
-    if(buttonState == HIGH){
-    digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-    Serial.println("LED is On");
-    delay(1000);              // wait for a second
-  } else {
-    digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-    Serial.println("LED is Off");
-    delay(1000);     // wait for a second
+  // make the current state of the switch (LOW or HIGH) into a local variable:
+  int reading = digitalRead(buttonPin);
+
+  // check to see if you just pressed the button 
+  // (i.e. the input went from LOW to HIGH), and that you've waited for longer than the lastDebounceTime:  
+
+  // If the switch changed:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  } 
+  
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    //take the actual current state of the button:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+        ledState = !ledState;
+      }
+    }
   }
+  
+  // set the LED:
+  digitalWrite(ledPin, ledState);
+
+  // save the reading.  Next time through the loop,
+  // it'll be the lastButtonState:
+  lastButtonState = reading;
 }
